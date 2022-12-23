6851a9e35e812eddb29e7e470a4d2b38568eaf67
Werechess <werechess@gmail.com>
2022-12-23 21:36:39 +0300

    Add allure steps to fillFormTests

--- src/test/java/com/demoqa/tests/DataGenerationPracticeFormTests.java
+++ src/test/java/com/demoqa/tests/DataGenerationPracticeFormTests.java
@@ -3,54 +3,76 @@ package com.demoqa.tests;
 import com.demoqa.pages.RegistrationFormPage;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
+
 class DataGenerationPracticeFormTests extends TestBase {
 
     private final RegistrationFormPage registrationFormPage = new RegistrationFormPage();
 
     @Test
     void fillFormWithMaximumDataTest() {
-        registrationFormPage.openPage()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setEmail(email)
-                .setGender(gender)
-                .setMobilePhone(mobilePhone)
-                .setDateOfBirth(dayOfBirth, monthOfBirth, yearOfBirth)
-                .setSubjects(subject)
-                .setHobby(firstHobby)
-                .setHobby(secondHobby)
-                .setHobby(thirdHobby)
-                .setPicture(picture)
-                .setAddress(address)
-                .setState(state)
-                .setCity(city)
-                .clickSubmit()
+        step("Open registration form", () -> {
+            registrationFormPage.openPage();
+        });
 
-                .checkResultsTableVisible()
-                .checkResult("Student Name", firstName + " " + lastName)
-                .checkResult("Student Email", email)
-                .checkResult("Gender", gender)
-                .checkResult("Mobile", mobilePhone)
-                .checkResult("Date of Birth", dayOfBirth + " " + monthOfBirth + "," + yearOfBirth)
-                .checkResult("Subjects", subject)
-                .checkResult("Hobbies", firstHobby + ", " + secondHobby + ", " + thirdHobby)
-                .checkResult("Picture", picture)
-                .checkResult("Address", address)
-                .checkResult("State and City", state + " " + city);
+        step("Fill registration form", () -> {
+            registrationFormPage.setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setEmail(email)
+                    .setGender(gender)
+                    .setMobilePhone(mobilePhone)
+                    .setDateOfBirth(dayOfBirth, monthOfBirth, yearOfBirth)
+                    .setSubjects(subject)
+                    .setHobby(firstHobby)
+                    .setHobby(secondHobby)
+                    .setHobby(thirdHobby)
+                    .setPicture(picture)
+                    .setAddress(address)
+                    .setState(state)
+                    .setCity(city);
+        });
+
+        step("Submit registration form", () -> {
+            registrationFormPage.clickSubmit();
+        });
+
+        step("Check results in opened table", () -> {
+            registrationFormPage.checkResultsTableVisible()
+                    .checkResult("Student Name", firstName + " " + lastName)
+                    .checkResult("Student Email", email)
+                    .checkResult("Gender", gender)
+                    .checkResult("Mobile", mobilePhone)
+                    .checkResult("Date of Birth", dayOfBirth + " " + monthOfBirth + "," + yearOfBirth)
+                    .checkResult("Subjects", subject)
+                    .checkResult("Hobbies", firstHobby + ", " + secondHobby + ", " + thirdHobby)
+                    .checkResult("Picture", picture)
+                    .checkResult("Address", address)
+                    .checkResult("State and City", state + " " + city);
+        });
     }
 
     @Test
     void fillFormWithMinimumDataTest() {
-        registrationFormPage.openPage()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setGender(gender)
-                .setMobilePhone(mobilePhone)
-                .clickSubmit()
+        step("Open registration form", () -> {
+            registrationFormPage.openPage();
+        });
 
-                .checkResultsTableVisible()
-                .checkResult("Student Name", firstName + " " + lastName)
-                .checkResult("Gender", gender)
-                .checkResult("Mobile", mobilePhone);
+        step("Fill registration form", () -> {
+            registrationFormPage.setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setGender(gender)
+                    .setMobilePhone(mobilePhone);
+        });
+
+        step("Submit registration form", () -> {
+            registrationFormPage.clickSubmit();
+        });
+
+        step("Check results in opened table", () -> {
+            registrationFormPage.checkResultsTableVisible()
+                    .checkResult("Student Name", firstName + " " + lastName)
+                    .checkResult("Gender", gender)
+                    .checkResult("Mobile", mobilePhone);
+        });
     }
 }
