4a7dbd11e1969cda90d842054d815d768a0acb44
Werechess <werechess@gmail.com>
2022-12-31 01:54:39 +0300

    Fix browserName property

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -52,7 +52,7 @@ public class TestBase {
         Configuration.browserCapabilities = capabilities;
         Configuration.holdBrowserOpen = false;
 
-        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browser = System.getProperty("browserName", "chrome");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
 
